4bf99a9be0a26d4d105fe67f35a55461fdf35278
bobbythree <mynameisbobbylewis@hotmail.com>
2025-04-26 14:16:12 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+### This will be a React app for building dnd parties.
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <>
+      <h1 className="text-2xl text-cyan-700">Hello Dudeman!!</h1>
+    </>
+  )
+}
+
