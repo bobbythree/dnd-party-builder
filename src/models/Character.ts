5aa4097bd5d57846a984748b647817c90920bf32
bobbythree <mynameisbobbylewis@hotmail.com>
2025-04-26 15:57:48 -0500

    add attributes to Char class

--- src/models/Character.ts
+++ src/models/Character.ts
@@ -1,7 +1,18 @@
 export class Character {
   name: string;
+  level: number;
+  race: string;
+  dnd_class: string;
 
-  constructor(name: string) {
+  constructor(
+    name: string,
+    level: number,
+    race: string,
+    dnd_class: string,
+  ) {
     this.name = name;
+    this.level = level;
+    this.race = race;
+    this.dnd_class = dnd_class;
   }
 }
