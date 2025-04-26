41d8c104ce06a252e36bdc48a8ecef3a36bedb83
bobbythree <mynameisbobbylewis@hotmail.com>
2025-04-26 17:26:40 -0500

    update druid class, create Weapon class

--- src/models/Druid.ts
+++ src/models/Druid.ts
@@ -2,15 +2,18 @@ import { Character } from './Character.ts'
 
 class Druid extends Character {
   spells: string[];
+  weapon: string;
 
   constructor(
     name: string,
     level: number,
     race: string,
     dnd_class: string,
-    spells: string[]
+    spells: string[],
+    weapon: string
   ) {
     super(name, level, race, dnd_class)
     this.spells = spells;
+    this.weapon = weapon;
   }
 }
---
+++ src/models/Weapon.ts
@@ -0,0 +1,9 @@
+class Weapon {
+  name: string;
+  damage: string;
+
+  constructor(name: string, damage: string) {
+    this.name = name;
+    this.damage = damage;
+  }
+}
