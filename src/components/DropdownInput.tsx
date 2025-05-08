70ddd3b6720d182a002656030c271047e9a8fb2f
bobbythree <mynameisbobbylewis@hotmail.com>
2025-05-07 20:46:50 -0500

    WIP making the dropdown work as intended

--- src/components/DropdownInput.tsx
+++ src/components/DropdownInput.tsx
@@ -1,12 +1,24 @@
+import { ChangeEvent } from "react";
+
 interface DropdownProps {
   label: string;
   options: string[];
+  name: string;
+  value: string;
+  onChange: (e: ChangeEvent<HTMLSelectElement>) => void;
 }
 
-export default function DropdownInput({ label, options }: DropdownProps) {
+export default function DropdownInput({ label, options, name, value, onChange }: DropdownProps) {
 
   return (
-    <select className="select" defaultValue={label}>
+    <select
+      className="select"
+      id={name}
+      defaultValue={label}
+      name={name}
+      value={value}
+      onChange={onChange}
+    >
       <option disabled={true}>{label}</option>
       {options.map((option, i) => (
         <option key={i}>{option}</option>
--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,14 +1,25 @@
+import { ChangeEvent } from "react";
+
 interface TextInputProps {
   label: string;
   placeholder?: string;
+  name: string;
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function TextInput({ label, placeholder }: TextInputProps) {
+export default function TextInput({ label, placeholder, name, value, onChange }: TextInputProps) {
   return (
     <>
-      <label className="input">
+      <label htmlFor={name} className="input">
         <span className="label text-neutral-100">{label}</span>
-        <input type="text" placeholder={placeholder} />
+        <input
+          type="text"
+          placeholder={placeholder}
+          id={name}
+          value={value}
+          onChange={onChange}
+        />
       </label>
     </>
   )
