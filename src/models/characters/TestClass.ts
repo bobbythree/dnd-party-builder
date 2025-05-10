a513c9305fe485df1880403a8e952887d4a22ef1
bobbythree <mynameisbobbylewis@hotmail.com>
2025-05-10 15:15:23 -0500

    add test class

---
+++ src/models/characters/TestClass.ts
@@ -0,0 +1,18 @@
+export class TestClass {
+  name: string;
+  race: string;
+  dnd_class: string;
+  alignment: string;
+
+  constructor(
+    name: string,
+    race: string,
+    dnd_class: string,
+    alignment: string,
+  ) {
+    this.name = name;
+    this.race = race;
+    this.dnd_class = dnd_class;
+    this.alignment = alignment;
+  }
+}
