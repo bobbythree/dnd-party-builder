6779cb62a0a86ae5f2f415c8a30479854f1a9571
bobbythree <mynameisbobbylewis@hotmail.com>
2025-05-17 18:56:18 -0500

    re-write context file

--- src/context/PartyContext.tsx
+++ src/context/PartyContext.tsx
@@ -1,24 +1,45 @@
-import { useState, createContext, useContext, ReactNode } from "react";
+import { useState, useRef, useMemo, createContext, useContext, ReactNode } from "react";
+import { Party } from "../models/Party.ts"
+import { Character } from "../models/characters/Character";
 
 // shape of context value
 interface PartyContextTypes {
-  partyName: string;
-  setPartyName: (name: string) => void;
+  party: Party;
+  setName: (name: string) => void;
+  addMember: (member: Character) => void;
 }
 
 // create context with initial value
 const PartyContext = createContext<PartyContextTypes | undefined>(undefined);
 
-// create provider
+// define prop types
 interface PartyProviderProps {
   children: ReactNode;
 }
 
+// create provider component
 export const PartyProvider = ({ children }: PartyProviderProps) => {
-  const [partyName, setPartyName] = useState<string>('');
-  const contextValue: PartyContextTypes = {
-    partyName, setPartyName,
-  }
+  const partyRef = useRef(new Party())
+
+  //create counter to trigger re-renders
+  const [counter, setCounter] = useState(0);
+
+  const setName = useMemo(() => (name: string) => {
+    partyRef.current.setName(name)
+    setCounter(prev => prev + 1)
+  }, []);
+
+  const addMember = useMemo(() => (member: Character) => {
+    partyRef.current.addMember(member)
+    setCounter(prev => prev + 1)
+  }, []);
+
+  //create context 
+  const contextValue = useMemo(() => ({
+    party: partyRef.current,
+    setName,
+    addMember,
+  }), [counter, setName, addMember]);
 
   return (
     <PartyContext.Provider value={contextValue}>
