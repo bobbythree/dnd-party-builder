392a2baaf3006fc6e6720d0a76cf07a094a3c685
bobbythree <mynameisbobbylewis@hotmail.com>
2025-05-19 17:27:33 -0500

    all pages responsive

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -48,7 +48,7 @@ export default function Welcome() {
           <p
             className=
             "text-center mt-[30vh] md:text-3xl text-xl">
-            Your party's name is:<br /> <br />
+            Your party's name is:<br className="mb-3" />
             <span className=
               "bg-linear-to-b from-warning to-red-800 text-transparent bg-clip-text">
               {party.name}
